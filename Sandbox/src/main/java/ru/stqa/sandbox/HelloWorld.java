ce95dcae16ae8cb105db21df76854960ba18292e
MarinaKablinova <37731945+MarinaKablinova@users.noreply.github.com>
2018-04-08 21:29:53 +0300

    add classes

--- Sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
+++ Sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
@@ -5,25 +5,25 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         hello("world");
-    double l = 6;
-    double a = 4;
-    double b = 54;
-    System.out.println("New area = " + area(a,b));
-    System.out.println("Площадь квадрата равна" + area(l));
+
+    Square s = new Square(5);
+
+    Rectangle r = new Rectangle(4, 6);
+    r.a = 4;
+    r.b = 6;
+    System.out.println("New area = " + area(r));
+    System.out.println("Площадь квадрата равна" + area(s));
     }
 
     public static void hello(String somebody){
-
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double l){
-        l = 36;
-        return l*l;
-
+    public static double area(Square s){
+        return s.l*s.l;
     }
 
-    public static double area(double a, double b){
-        return a*b;
+    public static double area(Rectangle r){
+        return r.a*r.b;
     }
 }
---
+++ Sandbox/src/main/java/ru/stqa/sandbox/Rectangle.java
@@ -0,0 +1,13 @@
+package ru.stqa.sandbox;
+
+public class Rectangle {
+    public  double a;
+    public double b;
+
+    public Rectangle(double a, double b)
+    {
+        //где this - это атрибут
+        this.a = a;
+        this.b = b;
+    }
+}
