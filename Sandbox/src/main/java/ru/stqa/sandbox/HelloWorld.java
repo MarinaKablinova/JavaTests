0e08ce6a9f04f060d90f7f5816006bc5013964bb
MarinaKablinova <37731945+MarinaKablinova@users.noreply.github.com>
2018-04-08 15:39:29 +0300

    Add several functions

--- Sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
+++ Sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
@@ -4,7 +4,26 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        hello("world");
+    double l = 6;
+    double a = 4;
+    double b = 54;
+    System.out.println("New area = " + area(a,b));
+    System.out.println("Площадь квадрата равна" + area(l));
     }
 
+    public static void hello(String somebody){
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l){
+        l = 36;
+        return l*l;
+
+    }
+
+    public static double area(double a, double b){
+        return a*b;
+    }
 }
